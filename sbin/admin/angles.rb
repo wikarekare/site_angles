c14535d87bffb7ec932c325f20698884ed5173c4
robertburrowes <r.burrowes@auckland.ac.nz>
2023-03-27 20:59:01 +1300

    explicit table.field names as strings, rather than using our custom mysql2 gem

--- sbin/admin/angles.rb
+++ sbin/admin/angles.rb
@@ -19,18 +19,19 @@ end
 
 def fetch_sites
   site_query = <<~SQL
-    SELECT c.site_name, c.latitude, c.longitude, c.height, d.site_name, d.latitude, d.longitude, d.height
-    FROM customer as c, distribution as d, customer_distribution
+    SELECT c.site_name as 'c.site_name', c.latitude as 'c.latitude', c.longitude as 'c.longitude', c.height as 'c.height',
+           d.site_name as 'd.site_name', d.latitude as 'd.latitude', d.longitude as 'd.longitude', d.height as 'd.height'
+    FROM customer AS c, distribution AS d, customer_distribution
     WHERE c.active = 1
     AND c.cabled = 0
     AND c.customer_id = customer_distribution.customer_id
     AND customer_distribution.distribution_id = d.distribution_id
     AND d.active = 1
-    ORDER BY d.site_name, c.longitude, c.latitude
+    ORDER BY 'd.site_name', 'c.longitude', 'c.latitude'
   SQL
 
   WIKK::SQL.connect(@mysql_conf) do |sql|
-    sql.each_hash(site_query, true) do |row|
+    sql.each_hash(site_query) do |row|
       elevation = lookup_elevation(lat: row['d.latitude'], long: row['d.longitude'])
       distribution_site = { site_name: row['d.site_name'], latitude: row['d.latitude'].to_f, longitude: row['d.longitude'].to_f, elevation: elevation + row['d.height'].to_f }
 
